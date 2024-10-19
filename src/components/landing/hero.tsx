9e3e71ee96d664fe4d58b3ce3ccffff2e8a03736
Ali <117570658+AliSabet1380@users.noreply.github.com>
2024-10-19 18:02:38 +0330

    Update hero.tsx

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section className="relative z-10 py-20 mt-20 lg:mt-36 w-full flex items-center flex-col justify-center mb-20">
+    <section className="relative z-10 py-20 mt-20 lg:mt-36 w-full flex items-center flex-col justify-center mb-36">
       <div className="absolute left-1/2 top-1/2 -z-10 h-[400px] w-[400px] lg:w-[800px] -translate-x-1/2 -translate-y-1/2 opacity-10 blur-[100px] bg-emerald-500"></div>
       <h1 className="text-center text-6xl font-bold leading-none md:text-[8rem] bg-gradient-to-r from-20% bg-clip-text text-transparent from-emerald-400 to-yellow-300 select-none">
         Recipe Platform
