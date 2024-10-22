a963de0decd864c2e3829bfd0f42148bd947d512
Ali Sbt <ali8551sbt@gmail.com>
2024-10-22 21:34:13 +0330

    Reset back

--- src/components/dashboard/new-item.tsx
+++ src/components/dashboard/new-item.tsx
@@ -3,15 +3,15 @@
 import { useNewItem } from "@/hooks/use-new-item";
 
 import { Button } from "@/components/ui/button";
-import { NewItemSheet } from "@/features/dashboard/components/new-item-sheet";
+import { NewItemSheet } from "@/features/recipes/components/new-item-sheet";
 
-export const NewItem = () => {
+export const NewItem = ({ userId }: { userId: string }) => {
   const { open } = useNewItem();
 
   return (
     <>
       <NewItemSheet />
-      <Button onClick={open}>Add New+</Button>
+      <Button onClick={() => open(userId)}>Add New+</Button>
     </>
   );
 };
--- src/hooks/use-new-item.ts
+++ src/hooks/use-new-item.ts
@@ -2,12 +2,14 @@ import { create } from "zustand";
 
 interface UseNewItem {
   isOpen: boolean;
-  open: () => void;
+  userId: string | undefined;
+  open: (userId: string) => void;
   close: () => void;
 }
 
 export const useNewItem = create<UseNewItem>((set) => ({
   isOpen: false,
-  open: () => set({ isOpen: true }),
+  userId: undefined,
+  open: (userId) => set({ isOpen: true, userId }),
   close: () => set({ isOpen: false }),
 }));
