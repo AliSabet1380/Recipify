80a2945f3be0cb428d55423e5db0edf7cf0d6195
Ali <117570658+AliSabet1380@users.noreply.github.com>
2024-11-25 23:30:54 +0330

    Update footer.tsx

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       <span className="text-center text-sm text-neutral-500">
         ©️ This Website created by Ali Sabet as parctice
       </span>
-      <span className="text-lg text-center">Oct 2024, Iran, Fars, Shirza</span>
+      <span className="text-lg text-center">Oct 2024, Iran, Fars, Shiraz</span>
       <div className="flex items-center space-x-5">
         <Link href={"https://github.com/AliSabet1380"}>
           <Image src={"/github.svg"} alt="github" width={25} height={25} />
